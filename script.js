abe8e36c8ecd184c2284ba706ae69548236fc574
Sujal Thapa <169607669+Sujal-thapaa@users.noreply.github.com>
2025-07-05 17:52:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// All JavaScript from index.html moved here. The Google Maps API script should still be loaded in the HTML with the callback=initMap.
+
+// ... existing code ...
+// (The entire <script>...</script> block from index.html goes here, except the <script src=...> for Google Maps) 
